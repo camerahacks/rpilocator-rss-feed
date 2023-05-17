de67b4769e8256a70f08dbff2f391fa5101940bc
bitiutsm <bitiutsm@tcd.ie>
2023-05-17 01:33:46 +0100

    Fix SSL certification failure

--- rpilocator-rss-desktop.py
+++ rpilocator-rss-desktop.py
@@ -1,6 +1,11 @@
 import feedparser
 import time
 from plyer import notification
+import ssl
+
+# Since the feed is using a self-signed certificate, we need to disable SSL verification
+# to prevent the script from throwing an error.
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Feed URL
 FEED_URL = 'https://rpilocator.com/feed/'
@@ -10,10 +15,11 @@ USER_AGENT = 'xlocator feed alert'
 
 print("Starting rpilocator-rss-desktop.py...")
 
-def notifyUser():
+# Noitfy the user about a new entry
+def notifyUser(user_message = 'The stock has been updated!'):
     notification.notify(
         title='Rpilocator Alert',
-        message='The stock has been updated!',
+        message=user_message,
         app_icon=None,
         timeout=10
     )
@@ -28,8 +34,10 @@ f = feedparser.parse(FEED_URL, agent=USER_AGENT)
 if f.entries:
     for entries in f.entries:
         control.append(entries.id)
+else:
+    print("No entries in feed.")
 
-#Only wait 30 seconds after initial run.
+# Only wait 30 seconds after initial run.
 print("Initial run. Waiting 30 seconds...")
 time.sleep(30)
 
@@ -38,14 +46,20 @@ while True:
     # Fetch the feed again, and again, and again...
     f = feedparser.parse(FEED_URL, agent=USER_AGENT)
 
-    # Compare feed entries to control list.
-    # If there are new entries, send a message/push
-    # and add the new entry to control variable
     for entry in f.entries:
         if entry.id not in control:
-            notifyUser()
+
+            # Set the message to the title of the entry
+            message = entry.title
+
+            # Duplicate the message to the console
+            print(message)
+
+            # Notify the user
+            notifyUser(message)
 
             # Add entry guid to the control variable
             control.append(entry.id)
 
+    # Wait 59 seconds before checking the feed again
     time.sleep(59)
